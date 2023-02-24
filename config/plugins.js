2772432607153c6fa7264b38b21bab770a80c010
taisumov <islam.taisumov10@gmail.com>
2023-02-24 23:07:03 +0300

    First commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,4 @@
 module.exports = ({ env }) => ({
-  'users-permissions': {
-    config: {
-      jwtSecret: env('JWT_SECRET'),
-    },
-  },
   'strapi-plugin-populate-deep': {
     config: {
       defaultDepth: 5, // Default is 5
