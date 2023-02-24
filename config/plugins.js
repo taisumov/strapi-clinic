205d929819afc7caaec70c8470e20aa8cdff8a17
taisumov <islam.taisumov10@gmail.com>
2023-02-24 22:54:55 +0300

    First commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,26 @@
 module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
   'strapi-plugin-populate-deep': {
     config: {
       defaultDepth: 5, // Default is 5
     }
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
   },
 });
